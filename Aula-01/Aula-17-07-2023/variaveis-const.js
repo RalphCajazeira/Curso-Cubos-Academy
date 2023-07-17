29ccc83f0cc6a3b25ee0bcec744c8c2168b93d83
RalphCajazeira <ralphmtk@gmail.com>
2023-07-17 18:33:33 -0300

    Criado a pasta para a Aula do dia 17-07-2023

---
+++ Aula-01/Aula-17-07-2023/variaveis-const.js
@@ -0,0 +1,11 @@
+//Criar uma variavel e guardar um string
+
+const primeiroNome = "Ralph";
+
+console.log(primeiroNome);
+
+let outroNome = "Messias";
+
+outroNome = "Fernanda"
+
+console.log(outroNome);
