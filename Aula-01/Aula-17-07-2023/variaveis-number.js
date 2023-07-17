a32eac942bac9b5e7796e36a46cbbb39ad537be7
RalphCajazeira <ralphmtk@gmail.com>
2023-07-17 18:35:19 -0300

    Exercicio Sobre Variaveis

---
+++ Aula-01/Aula-17-07-2023/variaveis-number.js
@@ -0,0 +1,9 @@
+let idade = 33;
+let alturaEmMetros = 1.73;
+
+console.log("Idade", idade, "Anos");
+console.log("Altura", alturaEmMetros, "Metros");
+
+//string template
+
+console.log(`Minha idade é ${idade} anos e minhha altura é ${alturaEmMetros} metros`);
