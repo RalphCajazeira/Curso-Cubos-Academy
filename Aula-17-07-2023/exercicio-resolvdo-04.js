a36bb5b5ec172126d05e76382074ac2690fa0e60
RalphCajazeira <ralphmtk@gmail.com>
2023-07-17 19:44:52 -0300

    Duplicad o arquivo ja tinha feito.

---
+++ Aula-17-07-2023/exercicio-resolvdo-04.js
@@ -0,0 +1,16 @@
+let x = 10, y = 2;
+let resultado;
+console.log(`O valor de x é ${x} e o valor de y é ${y}`)
+console.log("-----------------------")
+resultado = x + y;
+console.log(`A soma de x + y (${x} + ${y}) é = ${resultado}`)
+console.log("-----------------------")
+resultado = x - y;
+console.log(`A subtração de x - y (${x} - ${y}) é = ${resultado}`)
+console.log("-----------------------")
+resultado = x * y;
+console.log(`A multiplicação de x * y (${x} * ${y}) é = ${resultado}`)
+console.log("-----------------------")
+resultado = x / y;
+console.log(`A divisão de x / y (${x} / ${y}) é = ${resultado}`)
+console.log("-----------------------")
