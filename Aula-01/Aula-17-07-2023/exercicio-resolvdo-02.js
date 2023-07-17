1045afcc6f12877640f37635b046026b959cecee
RalphCajazeira <ralphmtk@gmail.com>
2023-07-17 18:34:27 -0300

    Exercicio Resolvido 02

---
+++ Aula-01/Aula-17-07-2023/exercicio-resolvdo-02.js
@@ -0,0 +1,13 @@
+// Declarar uma variavel mutavel
+// Guardar a população da cidade
+// Imprimir na tela
+// Modificar o valor
+// Imprimir Novamente
+
+let numeroDeHabitantes = 1_000_000
+
+console.log(numeroDeHabitantes, "Habitantes.");
+
+numeroDeHabitantes = 10
+
+console.log(numeroDeHabitantes, "Habitantes.");
