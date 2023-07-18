1f8b21256efe0e1f2caefa0606fcb430ad5827c7
RalphCajazeira <ralphmtk@gmail.com>
2023-07-18 18:12:07 -0300

    Resolvido Exercício 08 - Perímetro e Área

---
+++ Aula-17-07-2023/exercicios-backend-variaveiss/08/index.js
@@ -0,0 +1,19 @@
+// Input
+
+const raioCirculo = 1;
+
+let CircunferenciaCirculo;
+let areaCirculo;
+
+// Calculos
+
+//Area=πRaio²
+//Circunferencia Perimetro=2π.Raio
+
+areaCirculo = Math.PI * raioCirculo;
+CircunferenciaCirculo = 2 * Math.PI * raioCirculo;
+
+// Imprimir na Tela
+
+console.log(`A area do circulo é de ≅ ${areaCirculo} m²`);
+console.log(`A circunferência do circulo é de ≅ ${CircunferenciaCirculo} m`)
