8281443e23863df1e37e98c0581b351ed2f4e17a
RalphCajazeira <ralphmtk@gmail.com>
2023-07-18 17:21:43 -0300

    Resolvido Exercício 05 - Distancia

--- Aula-17-07-2023/exercicios-backend-variaveiss/05/index.js
+++ Aula-17-07-2023/exercicios-backend-variaveiss/05/index.js
@@ -1,1 +1,16 @@
+// Input
 
+const x1 = 1
+const x2 = 1
+const y1 = 1
+const y2 = 4
+
+let distancia;
+
+// Calculos
+
+distancia = Math.sqrt(Math.pow((x2 - x1) + (y2 - y1), 2));
+
+// Imprimir na Tela
+
+console.log(`A distancia é de ${distancia}`);
