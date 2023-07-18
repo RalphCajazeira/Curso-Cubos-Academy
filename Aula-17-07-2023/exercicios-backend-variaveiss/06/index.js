59394a0f543c8493386df45cca0344364cf2cf2b
RalphCajazeira <ralphmtk@gmail.com>
2023-07-18 17:31:13 -0300

    Rsolvido Execicio 06 - Velocidade Media

---
+++ Aula-17-07-2023/exercicios-backend-variaveiss/06/index.js
@@ -0,0 +1,14 @@
+// Input
+
+const distPecorridaEmMetros = 500;
+const tempoSegundos = 10;
+
+let velocidadeMedia;
+
+// Calculos
+
+velocidadeMedia = distPecorridaEmMetros / tempoSegundos * 3.6;
+
+// Imprimir na Tela
+
+console.log(`Sua velocidade media é de ${velocidadeMedia} km/h`);
