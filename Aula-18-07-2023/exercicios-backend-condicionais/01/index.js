5dd3bdac954ab781815afd0925c0f8feab1d5f1e
RalphCajazeira <ralphmtk@gmail.com>
2023-07-19 20:49:12 -0300

    Finalizado 01 - Pedra, papel ou tesoura

--- Aula-18-07-2023/exercicios-backend-condicionais/01/index.js
+++ Aula-18-07-2023/exercicios-backend-condicionais/01/index.js
@@ -1,4 +1,24 @@
-const jogada1 = "pedra"
-const jogada2 = "tesoura"
+const jogada1 = "papel"
+const jogada2 = "papel"
 
 //seu código aqui
+
+// 3 Possibilidades de Vitoria
+
+if (jogada1 === jogada2) {
+    // Verificar se é um Empate
+    console.log("Empate")
+} else if (
+    jogada1 !== "pedra" && jogada1 !== "papel" && jogada1 !== "tesoura" ||
+    jogada2 !== "pedra" && jogada2 !== "papel" && jogada2 !== "tesoura") {
+    console.log("Jogada inválida");
+} else if
+    // Verifica se jogador 1 ganha
+    ((jogada1 === "pedra" && jogada2 === "tesoura") ||
+    (jogada1 === "papel" && jogada2 === "pedra") ||
+    (jogada1 === "tesoura" && jogada2 === "papel")) {
+    console.log(jogada1)
+} else {
+    // Caso jogador 1 não ganhe
+    console.log(jogada2)
+}
