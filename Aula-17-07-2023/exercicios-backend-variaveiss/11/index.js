fece628641525f257ea39bc9bf11fcd66f93c841
RalphCajazeira <ralphmtk@gmail.com>
2023-07-19 09:44:12 -0300

    Resolvido Exercício 11 - Volume de uma Esfera

---
+++ Aula-17-07-2023/exercicios-backend-variaveiss/11/index.js
@@ -0,0 +1,18 @@
+// Input
+
+const diametroEsfera = 6
+
+let raioEsfera;
+let volumeEsfera;
+
+// Pre Calculos
+
+raioEsfera = diametroEsfera / 2;
+
+// Calculos
+
+volumeEsfera = 4 / 3 * Math.pow(raioEsfera, 3); //* Math.PI;
+
+// Imprimir na Tela
+
+console.log(`O volume da Esfera é ${volumeEsfera} PI`);
