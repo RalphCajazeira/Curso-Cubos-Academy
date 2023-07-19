c6805a33193a1aafd8d140aa9a0073ff6f7a6c03
RalphCajazeira <ralphmtk@gmail.com>
2023-07-19 10:36:40 -0300

    Resolvido Exercício 13 - Área total Cilindro.

---
+++ Aula-17-07-2023/exercicios-backend-variaveiss/13/index.js
@@ -0,0 +1,15 @@
+// Input
+
+const raioBase = 3
+const alturaCilindro = 1
+const PI = "PI"; //Só quis colocar aqui para jutar na Impressão da tela
+
+let areaTotalCilindro;
+
+// Calculos
+
+areaTotalCilindro = 2 * raioBase * (raioBase + alturaCilindro);
+
+// Imprimir na Tela
+
+console.log(`A área total do cilindro é ≅ ${areaTotalCilindro + PI} `);
