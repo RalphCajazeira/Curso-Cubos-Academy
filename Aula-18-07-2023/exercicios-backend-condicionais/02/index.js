dd7628d102fd6e1a4ccfd717e971d423413027ce
RalphCajazeira <ralphmtk@gmail.com>
2023-07-19 21:50:08 -0300

    Finalizado 02 - Par ou ímpar

--- Aula-18-07-2023/exercicios-backend-condicionais/02/index.js
+++ Aula-18-07-2023/exercicios-backend-condicionais/02/index.js
@@ -2,3 +2,11 @@ const jogada1 = 5;
 const jogada2 = 3;
 
 //seu código aqui
+
+let resultadoSoma = jogada1 + jogada2;
+
+if (resultadoSoma % 2 === 0) {
+    console.log("Numero é par")
+} else {
+    console.log("Numero é impar")
+}
