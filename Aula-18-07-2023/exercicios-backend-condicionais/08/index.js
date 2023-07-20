bbaedd53858027eeef1cc0641c6c08bb2ddd9b04
RalphCajazeira <ralphmtk@gmail.com>
2023-07-19 23:33:03 -0300

    Resolvido 08 - Montanha Russa Muito Assustadora

--- Aula-18-07-2023/exercicios-backend-condicionais/08/index.js
+++ Aula-18-07-2023/exercicios-backend-condicionais/08/index.js
@@ -3,3 +3,21 @@ const possuiPatologia = false;
 const altura = 180;
 const ehEstudante = false;
 
+// Verificações
+
+const alturaDeCorte = 150;
+
+// Menores de 12 anos
+// Maiores de 65 anos
+// Que possuam qualquer patologia cardíaca
+// Menores de 150cm de altura
+
+if (idade < 12 || idade > 65 || possuiPatologia || altura < alturaDeCorte) {
+    console.log("ACESSO NEGADO")
+    // } else if (idade < 18 || ehEstudante) {
+    //     console.log("10 reais")
+} else {
+    const precoEntrada = (idade < 18 || ehEstudante) ? "10 reais" : "20 reais";
+    console.log(precoEntrada)
+    // console.log("20 reais")
+}
