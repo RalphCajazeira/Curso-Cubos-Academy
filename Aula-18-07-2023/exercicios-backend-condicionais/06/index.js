9a63ca1e2ee5f4ce52dfd0565459e16f541fc74c
RalphCajazeira <ralphmtk@gmail.com>
2023-07-19 22:59:34 -0300

    Resolvido 06 - Peneira de Vôlei

--- Aula-18-07-2023/exercicios-backend-condicionais/06/index.js
+++ Aula-18-07-2023/exercicios-backend-condicionais/06/index.js
@@ -1,3 +1,13 @@
 const alturaEmCm = 185;
 
 //seu código aqui
+const alturaMinima = 180;
+
+if (alturaEmCm >= alturaMinima) {
+    if (alturaEmCm <= 185) console.log("LÍBERO")
+    else if (alturaEmCm <= 195) console.log("PONTEIRO")
+    else if (alturaEmCm <= 205) console.log("OPOSTO")
+    else if (alturaEmCm > 185) console.log("CENTRAL")
+} else {
+    console.log("REPROVADO")
+}
