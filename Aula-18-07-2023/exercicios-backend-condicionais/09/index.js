226a66967d78ac4cc54b21c041e118d896470e3a
RalphCajazeira <ralphmtk@gmail.com>
2023-07-19 23:41:27 -0300

    Resolvido 09 - Transformar nota em conceito

--- Aula-18-07-2023/exercicios-backend-condicionais/09/index.js
+++ Aula-18-07-2023/exercicios-backend-condicionais/09/index.js
@@ -1,1 +1,20 @@
-const nota = 8.5;
+const nota = 3;
+
+// Nota	Conceito
+// 9 a 10	A
+// 8 a 8,9	B
+// 6 a 7,9	C
+// 4 a 5,9	D
+// menos que 4	E
+// const nota = 8.5;
+// No exemplo acima, o seu programa deve imprimir a mensagem:
+
+//     O estudando obteve conceito B
+
+if (nota >= 9) { console.log("O estudando obteve conceito A") }
+else if (nota >= 8) { console.log("O estudando obteve conceito B") }
+else if (nota >= 6) { console.log("O estudando obteve conceito C") }
+else if (nota >= 4) { console.log("O estudando obteve conceito D") }
+else {
+    console.log("O estudando obteve conceito E")
+}
