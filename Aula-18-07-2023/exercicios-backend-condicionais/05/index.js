507d78eaf8d90582b72310b0a9b5972f3cc2cf8f
RalphCajazeira <ralphmtk@gmail.com>
2023-07-19 22:58:41 -0300

    Finalizado 06 - Peneira de Vôlei

--- Aula-18-07-2023/exercicios-backend-condicionais/05/index.js
+++ Aula-18-07-2023/exercicios-backend-condicionais/05/index.js
@@ -1,13 +1,10 @@
-const alturaEmCm = 206;
+const alturaEmCm = 185;
 
 //seu código aqui
 const alturaMinima = 180;
 
 if (alturaEmCm >= alturaMinima) {
-    if (alturaEmCm <= 185) console.log("LÍBERO")
-    else if (alturaEmCm <= 195) console.log("PONTEIRO")
-    else if (alturaEmCm <= 205) console.log("OPOSTO")
-    else if (alturaEmCm > 185) console.log("CENTRAL")
+    console.log("APROVADO")
 } else {
     console.log("REPROVADO")
 }
