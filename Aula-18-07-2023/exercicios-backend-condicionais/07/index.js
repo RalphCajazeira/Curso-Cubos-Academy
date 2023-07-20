75e52e561c467917354124007e813c4b8b837823
RalphCajazeira <ralphmtk@gmail.com>
2023-07-19 23:13:49 -0300

    Resolvido 07 - Isenção de impostos

--- Aula-18-07-2023/exercicios-backend-condicionais/07/index.js
+++ Aula-18-07-2023/exercicios-backend-condicionais/07/index.js
@@ -1,5 +1,16 @@
-const aposentada = false;
+const aposentada = true;
 const portadoraDeDoenca = false;
-const totalDeRendimentos = 3000000; //emCentavos
+const totalDeRendimentos = 2855970; //emCentavos
 
 //seu código aqui
+const corteDoLeao = 2855970;
+
+if (portadoraDeDoenca) {
+    console.log("ISENTA")
+} else if (aposentada) { // Percebi que foi dada a informação então eu decidi usar.
+    console.log("JÁ BASTA DE IMPOSTO COROA")
+} else if (totalDeRendimentos <= corteDoLeao) {
+    console.log("VAZA LEAO! JA TA DIFICIL SEM VOCE")
+} else {
+    console.log("PEGA LEAO")
+}
