fda8df8543036382e30779d7940b73537c66b244
RalphCajazeira <ralphmtk@gmail.com>
2023-07-21 09:40:54 -0300

    Resolvido 10 - Identificando caracteres

--- Aula-18-07-2023/exercicios-backend-condicionais/10/index.js
+++ Aula-18-07-2023/exercicios-backend-condicionais/10/index.js
@@ -1,1 +1,29 @@
-const caractere = "E";
+const caractere = "A";
+
+
+// Verificar se é uma vogal
+if (caractere === "a" || caractere === "A" ||
+    caractere === "e" || caractere === "E" ||
+    caractere === "i" || caractere === "I" ||
+    caractere === "o" || caractere === "O" ||
+    caractere === "u" || caractere === "U") {
+    if (caractere === caractere.toUpperCase()) {
+        console.log("Vogal maiuscula")
+    } else {
+        console.log("Vogal minuscula")
+    }
+    // Verificar se é uma consoante
+} else if ((caractere >= "a" && caractere <= "z") ||
+    (caractere >= "A" && caractere <= "Z")) {
+    if (caractere === caractere.toUpperCase()) {
+        console.log("Consoante Maiuscula")
+    } else {
+        console.log("Consoante Minuscula")
+    }
+    // Verifica se é um número
+} else if (!isNaN(caractere)) {
+    console.log("Número")
+    // E coloquei a mais se caso não for nenhuma opção acima
+} else {
+    console.log("Não é número, vogal ou consoante")
+}
