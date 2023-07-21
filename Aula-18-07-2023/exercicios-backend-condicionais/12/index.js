d13fa32326039247e61d805d26a966f191b0ce8e
RalphCajazeira <ralphmtk@gmail.com>
2023-07-21 09:57:14 -0300

    Resolvido 12 - Dia da Semana

--- Aula-18-07-2023/exercicios-backend-condicionais/12/index.js
+++ Aula-18-07-2023/exercicios-backend-condicionais/12/index.js
@@ -1,1 +1,31 @@
-const diaDaSemana = 3;
+const diaDaSemana = 7;
+
+const segunda = "Segunda Feira"
+const terca = "Terça Feira"
+const quarta = "Quarta Feira"
+const quinta = "Quinta Feira"
+const sexta = "Sexta Feira"
+const sabado = "Sábado"
+const domingo = "Domingo"
+
+if (diaDaSemana === 1) {
+    console.log(segunda)
+} else if (diaDaSemana === 2) {
+    console.log(terca)
+} else if (diaDaSemana === 3) {
+    console.log(quarta)
+} else if (diaDaSemana === 4) {
+    console.log(quinta)
+} else if (diaDaSemana === 5) {
+    console.log(sexta)
+} else if (diaDaSemana === 6) {
+    console.log(sabado)
+} else if (diaDaSemana === 7) {
+    console.log(domingo)
+} //else {
+//     console.log("A entrada deve ser um numero de 1 a 7")
+// }
+
+//Deixei comentato para fazer o commit eu antes de ler a segunda questão ja tinha
+//colocado essa verificação, estou criando o costume de tentar prever algumas coisas
+//e tentar não deixar aberturas para erros ou respostas inesperadas.
