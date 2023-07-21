89d226b181a156ea0b2d0a87cd2adb6966a3e71b
RalphCajazeira <ralphmtk@gmail.com>
2023-07-21 14:39:50 -0300

    Exercicio Resolvido 01 - Arrays

---
+++ Aula-19-07-2023/Exercicio-01.js
@@ -0,0 +1,16 @@
+
+
+const nomePaises = ['Brasil', 'Alemanha', 'Irlanda', 'Canada', 'EUA']
+console.log(nomePaises)
+console.log('-----------------------------')
+nomePaises.push('Australia')
+console.log(nomePaises)
+nomePaises.pop()
+console.log(nomePaises)
+nomePaises.unshift('Espanha')
+console.log(nomePaises)
+nomePaises.shift()
+console.log(nomePaises)
+console.log(nomePaises[nomePaises.length - 1])
+console.log(nomePaises[1])
+console.log(nomePaises[2])
