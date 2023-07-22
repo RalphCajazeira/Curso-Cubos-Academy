422be77d7f90de6cd8f791be303fc348f6cf115b
RalphCajazeira <ralphmtk@gmail.com>
2023-07-21 21:06:58 -0300

    Colocando Comentarios Para os Colegas

--- Aula-20-07-2023/03 - Cinco ou mais, um é grátis.js
+++ Aula-20-07-2023/03 - Cinco ou mais, um é grátis.js
@@ -2,9 +2,9 @@
 
 let valorProdutos = [100, 500, 100, 200, 50];
 
-let menorValor = valorProdutos[0]
-let totaProdutos = 0;
-let totalComDesconto;
+let menorValor = valorProdutos[0] // Iniciando essa variavel com o indice 0 do Array
+let totaProdutos = 0; // Usado para armazenar a soma dos numeros na Array
+let totalComDesconto; = // Iniciei essa variavel para usar ela depois para calcular o desconto
 
 // Calculos
 
@@ -17,14 +17,14 @@ for (const valor of valorProdutos) {
 
 // Subtraindo o valor do desconto no preço dos produtos
 
-totalComDesconto = totaProdutos - menorValor;
+totalComDesconto = totaProdutos - menorValor; // Usando a variavel criada la em cima
 
 // Imprimir na tela o valor a ser pago com o desconto
 
 console.log(totalComDesconto)
 
 
-// Console usado para ir fazendo as verificações
+// Console usado para ir fazendo as verificações durante os testes da criação do codigo
 // console.log(menorValor)
 // console.log(totaProdutos)
 // console.log(totalComDesconto)
