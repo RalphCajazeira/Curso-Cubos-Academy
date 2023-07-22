53fdb67fe56ecc172acf5c4dab15dafe39c6ea32
RalphCajazeira <ralphmtk@gmail.com>
2023-07-21 22:23:15 -0300

    17 - Extrato de Compra Online

--- Aula-18-07-2023/exercicios-backend-condicionais/17/index.js
+++ Aula-18-07-2023/exercicios-backend-condicionais/17/index.js
@@ -1,8 +1,19 @@
 //valor do produto comprado.
-const valorDoProduto = 100000;
+const valorDoProduto = 10000;
 
 //quantidade de parcelas
 const quantidadeDoParcelamento = 10;
 
 //valor pago
-const valorPago = 300;
+const valorPago = 3000;
+
+let valorDasParcelas = valorDoProduto / quantidadeDoParcelamento;
+let parcelasRestantes = quantidadeDoParcelamento - (valorPago * quantidadeDoParcelamento / valorDoProduto);
+
+
+console.log(`Restam ${parcelasRestantes} parcelas de R$ ${valorDasParcelas.toFixed(2)}`)
+
+
+
+// console.log(valorDasParcelas)
+// console.log(parcelasRestantes)
