2d7984130b4e056c0436ff3be631dec1fbbf3e69
RalphCajazeira <ralphmtk@gmail.com>
2023-07-21 21:32:01 -0300

    Corrigido erro de quantidade minima

--- Aula-20-07-2023/03 - Cinco ou mais, um é grátis.js
+++ Aula-20-07-2023/03 - Cinco ou mais, um é grátis.js
@@ -1,30 +1,37 @@
 //Input
 
-let valorProdutos = [100, 500, 100, 200, 50];
+let precos = [100, 100, 100, 50];
 
-let menorValor = valorProdutos[0] // Iniciando essa variavel com o indice 0 do Array
+let menorValor = precos[0] // Iniciando essa variavel com o indice 0 do Array
 let totaProdutos = 0; // Usado para armazenar a soma dos numeros na Array
-let totalComDesconto; = // Iniciei essa variavel para usar ela depois para calcular o desconto
+let totalComDesconto; // Iniciei essa variavel para usar ela depois para calcular o desconto
+let descontoAcimaDeXProdutos = 5; // Numero de produtos para implementar o desconto
 
 // Calculos
 
-for (const valor of valorProdutos) {
+for (const valor of precos) {
     totaProdutos += valor
     if (valor < menorValor) {
         menorValor = valor;
     }
 }
 
-// Subtraindo o valor do desconto no preço dos produtos
+// Subtraindo o valor do desconto no preco dos produtos
 
 totalComDesconto = totaProdutos - menorValor; // Usando a variavel criada la em cima
 
 // Imprimir na tela o valor a ser pago com o desconto
 
-console.log(totalComDesconto)
+if (precos.length >= descontoAcimaDeXProdutos) {
+    console.log(totalComDesconto)
+} else {
+    console.log(totaProdutos)
+}
 
 
-// Console usado para ir fazendo as verificações durante os testes da criação do codigo
+
+// Console usado para ir fazendo as verificacoes durante os testes da criacao do codigo
 // console.log(menorValor)
 // console.log(totaProdutos)
 // console.log(totalComDesconto)
+// console.log(precos.length)
