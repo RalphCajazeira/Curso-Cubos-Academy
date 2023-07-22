d905a63890d544e241eaea3960e6abd779d74af8
RalphCajazeira <ralphmtk@gmail.com>
2023-07-21 23:21:17 -0300

    02 - Contando letras

--- Aula-19-07-2023/exercicios-backend-arrays-loops/02/index.js
+++ Aula-19-07-2023/exercicios-backend-arrays-loops/02/index.js
@@ -1,1 +1,17 @@
-const letras = ["A", "a", "B", "C", "E", "e"];
+const letras = ["A", "e", "B", "C", "E", "z"];
+
+
+const buscarLetra = 'e';
+let contarLetrasAchadas = 0;
+
+for (let letra of letras) {
+    if (letra.toLowerCase() === buscarLetra.toLowerCase()) {
+        contarLetrasAchadas++
+    }
+}
+
+if (contarLetrasAchadas > 0) {
+    console.log(`Foram encontradas ${contarLetrasAchadas} letras ${buscarLetra.toUpperCase()} ou ${buscarLetra.toLowerCase()}.`)
+} else {
+    console.log(`Nenhuma letra ${buscarLetra.toUpperCase()} ou ${buscarLetra.toLowerCase()} foi encontrada.`)
+}
