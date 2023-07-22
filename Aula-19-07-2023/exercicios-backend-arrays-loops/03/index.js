a170621cd45993fd775d6c4aa2c429a97ece3042
RalphCajazeira <ralphmtk@gmail.com>
2023-07-21 23:36:55 -0300

    03 - Encontre o 10

--- Aula-19-07-2023/exercicios-backend-arrays-loops/03/index.js
+++ Aula-19-07-2023/exercicios-backend-arrays-loops/03/index.js
@@ -1,2 +1,16 @@
-const numeros = [54, 22, 14, 87, 10, 284];
+const numeros = [54, 22, 14, 87, 284];
+// const numeros = [54, 22, 14, 10, 284];
 
+const acharNumero = 10;
+let posicaoNumeroX = -1;
+let indice = 0;
+
+for (const numero of numeros) {
+    if (numero === 10) {
+        posicaoNumeroX = indice;
+        break;
+    }
+    indice++;
+}
+
+console.log(posicaoNumeroX);
