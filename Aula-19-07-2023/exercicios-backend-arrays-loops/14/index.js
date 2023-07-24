60b3c71d192ae9c7ca2c63622b9d4aa737e2ec3b
RalphCajazeira <ralphmtk@gmail.com>
2023-07-24 13:37:37 -0300

    14 - Imprima os menores

--- Aula-19-07-2023/exercicios-backend-arrays-loops/14/index.js
+++ Aula-19-07-2023/exercicios-backend-arrays-loops/14/index.js
@@ -1,3 +1,12 @@
-const arrayA = [5, 32, 3, 44, 1];
-const arrayB = [57, 4, 21, 2, 13];
+// const arrayA = [5, 32, 3, 44, 1];
+// const arrayB = [57, 4, 21, 2, 13];
 
+const arrayA = [1, 3, 3, 44, 11];
+const arrayB = [57, 4, 21, 32, 13];
+
+// seu codigo aqui
+
+
+for (let i = 0; i < arrayA.length; i++) {
+    console.log(Math.min((arrayA[i]), arrayB[i]))
+}
