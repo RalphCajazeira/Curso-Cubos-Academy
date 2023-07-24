38919b97f513d09f395f50fe79fdec3ef29c8d2a
RalphCajazeira <ralphmtk@gmail.com>
2023-07-24 10:24:44 -0300

    05 - Filtrando apenas os pares

--- Aula-19-07-2023/exercicios-backend-arrays-loops/05/index.js
+++ Aula-19-07-2023/exercicios-backend-arrays-loops/05/index.js
@@ -1,1 +1,16 @@
 const original = [1, 4, 12, 21, 53, 88, 112];
+
+
+// input
+
+const numerosPares = [];
+
+// Calculos
+
+for (let i of original) {
+    if (i % 2 === 0) {
+        numerosPares.push(i)
+    }
+}
+
+console.log(numerosPares)
