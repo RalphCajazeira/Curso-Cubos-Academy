6e5efd354fb038089a925b4f343523a6a0be2461
RalphCajazeira <ralphmtk@gmail.com>
2023-07-24 11:22:59 -0300

    12 - Fila do banco

--- Aula-19-07-2023/exercicios-backend-arrays-loops/12/index.js
+++ Aula-19-07-2023/exercicios-backend-arrays-loops/12/index.js
@@ -1,3 +1,26 @@
-const filaDeDentro = ["Jose", "Maria", "Joao"];
-const filaDeFora = ["Joana", "Roberta", "Marcos", "Felipe"];
+// const filaDeDentro = ["Jose", "Maria", "Joao"];
+// const filaDeFora = ["Joana", "Roberta", "Marcos", "Felipe"];
 
+const filaDeDentro = ["Jose", "Joao"];
+const filaDeFora = ["Joana", "Roberta"];
+
+
+while (filaDeDentro.length < 5 && filaDeFora.length > 0) {
+    filaDeDentro.push(filaDeFora[0])
+    filaDeFora.shift()
+}
+
+
+
+// if (filaDeDentro.length < 5 && filaDeFora.length > 0) {
+//     filaDeDentro.push(filaDeFora[0])
+//     filaDeFora.shift()
+// }
+
+
+
+
+// console.log(filaDeDentro.length)
+// console.log(filaDeFora.length)
+console.log(filaDeDentro)
+console.log(filaDeFora)
