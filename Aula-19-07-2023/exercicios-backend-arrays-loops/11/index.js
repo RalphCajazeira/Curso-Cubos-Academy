cecc7df4f50517150a963514a55e74a3b76da27f
RalphCajazeira <ralphmtk@gmail.com>
2023-07-24 11:05:26 -0300

    11 - Filtrando números

--- Aula-19-07-2023/exercicios-backend-arrays-loops/11/index.js
+++ Aula-19-07-2023/exercicios-backend-arrays-loops/11/index.js
@@ -1,2 +1,15 @@
-const original = [5, 7, 13, 17, 26, 34, 118, 245];
+// const original = [5, 7, 13, 17, 26, 34, 118, 245];
+const original = [5, 7, 10, 13, 17, 21, 26, 34, 100, 118, 245];
 
+// seu codigo aqui
+let novaOriginal = [];
+
+for (let i of original) {
+    if (i >= 10 && i <= 20) {
+        novaOriginal.push(i)
+    } else if (i > 100) {
+        novaOriginal.push(i)
+    }
+}
+
+console.log(novaOriginal)
