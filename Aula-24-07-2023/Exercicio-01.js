1cef0c43c1810a904406ade32347e119e9b62d5c
RalphCajazeira <ralphmtk@gmail.com>
2023-07-24 17:56:57 -0300

    Melhorando o codigo - Apelidos

--- Aula-24-07-2023/Exercicio-01.js
+++ Aula-24-07-2023/Exercicio-01.js
@@ -5,7 +5,7 @@ const pessoa = {
     idade: 33,
     altura: 1.72,
     temCNH: true,
-    apelidos: ["MTK", "TK"]
+    apelidos: ["MTK", "TK", "Trakino"]
 
 }
 let textoCNH = "";
@@ -13,6 +13,7 @@ let textoCNH = "";
 textoCNH = pessoa.temCNH ? "possui CNH" : "não possui CNH";
 textoApelidos = pessoa.apelidos.length === 1 ? "o apelido é" : "seus apelidos são";
 
-console.log(`${pessoa.nome} tem ${pessoa.idade} anos ${pessoa.altura}m ${textoCNH} e ${textoApelidos}:
-- ${pessoa.apelidos[0]}
-- ${pessoa.apelidos[1]}`)
+console.log(`${pessoa.nome} tem ${pessoa.idade} anos ${pessoa.altura}m ${textoCNH} e ${textoApelidos}:`)
+for (let i of pessoa.apelidos) {
+    console.log(`- ${i}`)
+}
