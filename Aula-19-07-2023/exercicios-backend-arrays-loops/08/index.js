06ca546114fba4b550766bddb35806fd887b8ce8
RalphCajazeira <ralphmtk@gmail.com>
2023-07-24 10:45:05 -0300

    08 - Encontrando o maior

--- Aula-19-07-2023/exercicios-backend-arrays-loops/08/index.js
+++ Aula-19-07-2023/exercicios-backend-arrays-loops/08/index.js
@@ -1,1 +1,11 @@
 const numeros = [3, 24, 1, 8, 11, 7, 15];
+
+let maiorNumero = numeros[0];
+
+for (let i of numeros) {
+    if (i > maiorNumero) {
+        maiorNumero = i
+    }
+}
+
+console.log(maiorNumero)
