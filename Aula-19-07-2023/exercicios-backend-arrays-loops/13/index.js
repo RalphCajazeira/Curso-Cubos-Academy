e29f987185813b8fabf063515e4a1a13dfd91d86
RalphCajazeira <ralphmtk@gmail.com>
2023-07-24 11:37:53 -0300

    13 - Verificando disjuntores

--- Aula-19-07-2023/exercicios-backend-arrays-loops/13/index.js
+++ Aula-19-07-2023/exercicios-backend-arrays-loops/13/index.js
@@ -1,2 +1,26 @@
-const disjuntores = [false, false, true, false, false, true, false, false];
+// const disjuntores = [false, false, true, false, false, true, false, false];
+const disjuntores = [
+    false,
+    true,
+    true,
+    false,
+    false,
+    true,
+    false,
+    false,
+    true,
+    true,
+];
+
+// seu codigo aqui
+
+
+for (const [i, v] of disjuntores.entries()) {
+    if (v) {
+        console.log(i)
+    }
+}
+
+// console.log(i)
+// console.log(v)
 
