44c74f782d0f38c7bfe99e04e218fd1b883d2329
RalphCajazeira <ralphmtk@gmail.com>
2023-07-24 10:54:42 -0300

    09 - Fila de pedidos

--- Aula-19-07-2023/exercicios-backend-arrays-loops/09/index.js
+++ Aula-19-07-2023/exercicios-backend-arrays-loops/09/index.js
@@ -1,2 +1,11 @@
 
 const filaDePedidos = ["pedido 12", "pedido 13", "pedido 14"];
+
+// Calculos
+
+filaDePedidos.shift()
+filaDePedidos.push("pedido 15")
+
+// Imprimir na Tela
+
+console.log(filaDePedidos)
