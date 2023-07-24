cc1f4ebc87958cb159e94e9e0b775b4b8d4c2829
RalphCajazeira <ralphmtk@gmail.com>
2023-07-24 10:41:21 -0300

    07 - Filtrando nomes com a letra A

--- Aula-19-07-2023/exercicios-backend-arrays-loops/07/index.js
+++ Aula-19-07-2023/exercicios-backend-arrays-loops/07/index.js
@@ -1,1 +1,14 @@
-const nomes = ["Ana", "Joana", "Carlos", "amanda"];
+const nomes = ["Ana", "Joana", "Carlos", "amanda", "Angela"];
+
+let filtroLetra = "A";
+let listaFiltro = [];
+
+filtroLetra = filtroLetra.toUpperCase();
+
+for (let i of nomes) {
+    if (i[0].toUpperCase() === filtroLetra.toUpperCase()) {
+        listaFiltro.push(i)
+    }
+}
+
+console.log(listaFiltro)
