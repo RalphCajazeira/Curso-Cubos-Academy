3e1cd3cc70656fee75e6733dae784275375ac322
RalphCajazeira <ralphmtk@gmail.com>
2023-07-25 19:12:23 -0300

    11 - Valor da parcela do Sucesso Compartilhado

--- Aula-18-07-2023/exercicios-backend-condicionais/11/index.js
+++ Aula-18-07-2023/exercicios-backend-condicionais/11/index.js
@@ -1,22 +1,18 @@
-//renda mensal do aluno, em centavos.
+// //renda mensal do aluno, em centavos.
 const rendaMensalEmCentavos = 300000;
 
-// Tempo decorrido de contrato. Se for maior que 60 meses, o aluno não deve mais nada.
+// // Tempo decorrido de contrato. Se for maior que 60 meses, o aluno não deve mais nada.
 const mesesDecorridos = 12;
 
-// Soma das parcelas já pagas pelo aluno nos meses anteriores (em centavos). Se for igual a 18 mil reais, o aluno não deve pagar mais nada, pois já quitou a dívida.
+// // Soma das parcelas já pagas pelo aluno nos meses anteriores (em centavos). Se for igual a 18 mil reais, o aluno não deve pagar mais nada, pois já quitou a dívida.
 const totalJaPagoPeloAluno = 1000000;
 
 
-// Inicio
-let valorDaParcela = rendaMensalEmCentavos * 0.18;
+const renda = 200000
+let parcelaMensal = 0;
+const porcentagemParcela = 18 / 100;
 
-if (valorDaParcela > totalJaPagoPeloAluno) {
-    valorDaParcela = totalJaPagoPeloAluno;
+if (rendaMensalEmCentavos > renda) {
+    parcelaMensal = rendaMensalEmCentavos * porcentagemParcela
+    console.log(parcelaMensal)
 }
-
-if (mesesDecorridos > 60) {
-    valorDaParcela = 0;
-}
-
-console.log(`O valor da parcela desse mês é R$ ${valorDaParcela / 100}`)
