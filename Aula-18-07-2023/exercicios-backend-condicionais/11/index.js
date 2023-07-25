1a5c0b902d55dad2e7fa63f081390d57699d397b
RalphCajazeira <ralphmtk@gmail.com>
2023-07-25 19:25:38 -0300

    11 - Feito a letra B

--- Aula-18-07-2023/exercicios-backend-condicionais/11/index.js
+++ Aula-18-07-2023/exercicios-backend-condicionais/11/index.js
@@ -1,5 +1,5 @@
 // //renda mensal do aluno, em centavos.
-const rendaMensalEmCentavos = 300000;
+const rendaMensalEmCentavos = 300;
 
 // // Tempo decorrido de contrato. Se for maior que 60 meses, o aluno não deve mais nada.
 const mesesDecorridos = 12;
@@ -12,7 +12,11 @@ const renda = 200000
 let parcelaMensal = 0;
 const porcentagemParcela = 18 / 100;
 
+// mesesDecorridos < 60 && totalJaPagoPeloAluno < 1800000) 
+
 if (rendaMensalEmCentavos > renda) {
-    parcelaMensal = rendaMensalEmCentavos * porcentagemParcela
-    console.log(parcelaMensal)
+    parcelaMensal = rendaMensalEmCentavos * porcentagemParcela;
+    console.log(`R$ ${parcelaMensal.toFixed(2)}`)
+} else {
+    console.log("O valor da parcela desse mês é R$ 0 reais. Nenhum valor é devido pois a renda do estudante está abaixo do valor mínimo de R$ 2000 reais.")
 }
