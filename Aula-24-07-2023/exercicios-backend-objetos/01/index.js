6bdc56743cf20fbe474aa1bf92836387c7b2af95
RalphCajazeira <ralphmtk@gmail.com>
2023-07-26 20:47:14 -0300

    01 - Cadastro de usuário

---
+++ Aula-24-07-2023/exercicios-backend-objetos/01/index.js
@@ -0,0 +1,22 @@
+
+
+const endereco = {
+    rua: "Rua da Paz",
+    numero: 123,
+    complemento: "Apto. 456",
+    bairro: "Centro",
+    cep: "01234-567",
+    cidade: "São Paulo",
+    estado: "SP",
+    pais: "Brasil",
+};
+
+const usuario = {
+    nome: "João da Silva",
+    email: "joao.da.silva@gmail.com",
+    telefone: "(11) 98765-4321",
+    data_nascimento: "12/03/1990",
+    endereco: endereco,
+};
+
+console.log(usuario);
