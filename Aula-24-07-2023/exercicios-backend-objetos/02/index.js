cb9d6c46bfc98f471f02b7f3f9d96e5bc9a6fdc3
RalphCajazeira <ralphmtk@gmail.com>
2023-07-26 20:59:57 -0300

    02 - Lista de veículos

---
+++ Aula-24-07-2023/exercicios-backend-objetos/02/index.js
@@ -0,0 +1,46 @@
+
+
+const carros = [
+    {
+        marca: "Volkswagen",
+        modelo: "Gol",
+        ano: 2022,
+        cor: "vermelho",
+        quantidade_portas: 4,
+        automatico: false
+    },
+    {
+        marca: "Fiat",
+        modelo: "Uno",
+        ano: 2022,
+        cor: "prata",
+        quantidade_portas: 4,
+        automatico: false
+    },
+    {
+        marca: "Chevrolet",
+        modelo: "Onix",
+        ano: 2021,
+        cor: "preto",
+        quantidade_portas: 4,
+        automatico: true
+    },
+    {
+        marca: "Honda",
+        modelo: "Civic",
+        ano: 2020,
+        cor: "branco",
+        quantidade_portas: 4,
+        automatico: true
+    },
+    {
+        marca: "Toyota",
+        modelo: "Corolla",
+        ano: 2019,
+        cor: "cinza",
+        quantidade_portas: 4,
+        automatico: false
+    },
+];
+
+console.log(carros)
