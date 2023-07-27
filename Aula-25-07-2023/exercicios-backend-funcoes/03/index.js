0a7befd9ff434229a73a9b9a4eec2434c98f3869
RalphCajazeira <ralphmtk@gmail.com>
2023-07-27 14:48:33 -0300

    Funcao isolada addProdutoAoCarrinho

--- Aula-25-07-2023/exercicios-backend-funcoes/03/index.js
+++ Aula-25-07-2023/exercicios-backend-funcoes/03/index.js
@@ -13,22 +13,25 @@ const carrinho = {
             qtd: 2,
             precoUnit: 5000
         }
-    ]
-}
+    ],
+    imprimirResumo: function () {
 
-function imprimirResumoDoCarrinho(carrinho) {
+        let precoTotal = 0; // Variavel para guardar valor total do carrinho
+        let quantItem = 0; // Variavel para guardar quantidade de itens no carrinho
 
-    let precoTotal = 0; // Variavel para guardar valor total do carrinho
-    let quantItem = 0; // Variavel para guardar quantidade de itens no carrinho
+        for (let i of this.produtos) {
+            precoTotal += i.qtd * i.precoUnit;
+            quantItem += i.qtd
+        }
 
-    for (let i of carrinho.produtos) {
-        precoTotal += i.qtd * i.precoUnit;
-        quantItem += i.qtd
+        console.log(`Cliente: ${this.nomeDoCliente}`)
+        console.log(`Total de itens: ${quantItem} itens`)
+        console.log(`Total a pagar: R$ ${precoTotal.toFixed(2) / 100}`)
     }
-
-    console.log(`Cliente: ${carrinho.nomeDoCliente}`)
-    console.log(`Total de itens: ${quantItem} itens`)
-    console.log(`Total a pagar: R$ ${precoTotal.toFixed(2) / 100}`)
 }
 
-imprimirResumoDoCarrinho(carrinho) // Chama a funcao carrinho
+carrinho.imprimirResumo() // Chama o metodo carrinho
+
+function addProdutoAoCarrinho(carrinho, produto) {
+
+}
