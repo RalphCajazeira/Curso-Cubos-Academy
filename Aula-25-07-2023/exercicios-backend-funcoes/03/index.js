34f4cf1a4d1b20bb442230f991f6df745ac2894b
RalphCajazeira <ralphmtk@gmail.com>
2023-07-27 14:43:25 -0300

    Resolvido A e B

--- Aula-25-07-2023/exercicios-backend-funcoes/03/index.js
+++ Aula-25-07-2023/exercicios-backend-funcoes/03/index.js
@@ -15,3 +15,20 @@ const carrinho = {
         }
     ]
 }
+
+function imprimirResumoDoCarrinho(carrinho) {
+
+    let precoTotal = 0; // Variavel para guardar valor total do carrinho
+    let quantItem = 0; // Variavel para guardar quantidade de itens no carrinho
+
+    for (let i of carrinho.produtos) {
+        precoTotal += i.qtd * i.precoUnit;
+        quantItem += i.qtd
+    }
+
+    console.log(`Cliente: ${carrinho.nomeDoCliente}`)
+    console.log(`Total de itens: ${quantItem} itens`)
+    console.log(`Total a pagar: R$ ${precoTotal.toFixed(2) / 100}`)
+}
+
+imprimirResumoDoCarrinho(carrinho) // Chama a funcao carrinho
