59e833b320ef8654ddd65584dfbf917987add165
RalphCajazeira <ralphmtk@gmail.com>
2023-07-27 09:34:27 -0300

    05 - Cadastro de aulas

---
+++ Aula-24-07-2023/exercicios-backend-objetos/05/index.js
@@ -0,0 +1,31 @@
+
+
+
+
+const curso = {
+    id: 1234,
+    nome: "Lógica de programação",
+    aulas: []
+}
+const aulas = [
+    {
+        Identificador: 1,
+        nome_da_aula: "Introdução a programação"
+    },
+    {
+        Identificador: 2,
+        nome_da_aula: "Variáveis"
+    },
+    {
+        Identificador: 3,
+        nome_da_aula: "Condicionais"
+    },
+    {
+        Identificador: 4,
+        nome_da_aula: "Arrays"
+    }
+]
+
+curso.aulas = aulas
+
+console.log(curso)
