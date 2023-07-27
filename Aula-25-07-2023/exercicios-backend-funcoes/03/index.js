ddb601f78bd3de26594699210818d4f0b04a2e8f
RalphCajazeira <ralphmtk@gmail.com>
2023-07-27 16:18:12 -0300

    Letra e Resolvida

--- Aula-25-07-2023/exercicios-backend-funcoes/03/index.js
+++ Aula-25-07-2023/exercicios-backend-funcoes/03/index.js
@@ -30,8 +30,37 @@ const carrinho = {
     }
 }
 
-carrinho.imprimirResumo() // Chama o metodo carrinho
-
 function addProdutoAoCarrinho(carrinho, produto) {
-
+    let itemNoCarrinho = false // Validação de item no carrinho
+    // Verificar se intem estah no carrinho se tiver soma as quantidades.
+    for (let i of carrinho.produtos) {
+        if (i.id === produto.id) {
+            i.qtd += produto.qtd
+            console.log('Colocando como true variavel de item existente')
+            itemNoCarrinho = true
+        }
+    }
+    // Se o item não tiver no carrinho adiciona o item no carrinho na lista produtos.
+    if (!itemNoCarrinho) {
+        carrinho.produtos.push(produto)
+    }
 }
+
+const novaBermuda = {
+    id: 2,
+    nome: "Bermuda",
+    qtd: 3,
+    precoUnit: 5000
+}
+
+addProdutoAoCarrinho(carrinho, novaBermuda);
+
+const novoTenis = {
+    id: 3,
+    nome: "Tenis",
+    qtd: 1,
+    precoUnit: 10000
+}
+
+addProdutoAoCarrinho(carrinho, novoTenis);
+carrinho.imprimirResumo();
