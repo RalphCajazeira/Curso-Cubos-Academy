910ab41cf71bbad57ce20d681deec8b988bfc6b6
RalphCajazeira <ralphmtk@gmail.com>
2023-08-01 18:32:01 -0300

    01 - Sistema de filtro de comentários

--- Aula-27-07-2023/exercicios-backend-metodos-strings/01/index.js
+++ Aula-27-07-2023/exercicios-backend-metodos-strings/01/index.js
@@ -1,1 +1,25 @@
-const comentario = "Esse COVID é muito perigoso!";
+const comentario = "PandeMia Esse COVID é muito perigoso!";
+const palavrasProibidas = ['pandemiaa', 'covidd', 'perigoso'];
+
+const comentarioUpperCase = comentario.toLocaleUpperCase()
+const arrayComentario = comentario.split(' ')
+let palabrasUsadasProibidas = [];
+
+// console.log(arrayComentario)
+
+// const palavraProibida = comentario.includes(palavrasProibidas)
+
+// console.log(palavraProibida)
+
+for (let i = 0; i < palavrasProibidas.length; i++) {
+    let filtro = palavrasProibidas[i].toLocaleUpperCase()
+    if (comentarioUpperCase.includes(filtro)) {
+        palabrasUsadasProibidas.push(filtro)
+    }
+}
+
+if (palabrasUsadasProibidas.length > 0) {
+    console.log(`Comentário bloqueado por conter palavras proibidas,${palabrasUsadasProibidas}`)
+} else {
+    console.log('Comentário autorizado')
+}
