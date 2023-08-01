ce3aa570c5491fbb3e1718035e6b8787bdb7ec12
RalphCajazeira <ralphmtk@gmail.com>
2023-08-01 19:38:30 -0300

    05 - Esconder número do cartão de crédito

--- Aula-27-07-2023/exercicios-backend-metodos-strings/05/index.js
+++ Aula-27-07-2023/exercicios-backend-metodos-strings/05/index.js
@@ -1,1 +1,7 @@
 const numeroCartao = '1111222233334444';
+
+const numeroOculto = numeroCartao.slice(4, 12).replace(/\d/g, '*')
+const numeroFormatado = numeroCartao.slice(0, 4) + numeroOculto + numeroCartao.slice(12)
+
+// console.log(numeroOculto)
+console.log(numeroFormatado)
