117a19b02789df985acf5d4e4aa6fd417dd7ef9e
RalphCajazeira <ralphmtk@gmail.com>
2023-08-01 19:28:57 -0300

    04 - Sistema de formatação de dados do formulário

--- Aula-27-07-2023/exercicios-backend-metodos-strings/04/index.js
+++ Aula-27-07-2023/exercicios-backend-metodos-strings/04/index.js
@@ -1,3 +1,34 @@
 let identificador = "123";
 let nome = "José silva costa";
 let email = "      jose@email.com  ";
+
+// console.log(identificador.padStart(6, '0'))
+
+// const arrayDeNomes = nome.split(' ')
+// let nomeFormatado = ''
+
+// for (let item of arrayDeNomes) {
+//     let primeiraLetra = item[0]
+//     let restoDoNome = item.slice(1)
+//     nomeFormatado += primeiraLetra.toLocaleUpperCase() + restoDoNome + ' '
+// }
+
+// console.log(nomeFormatado)
+// console.log(email.trim())
+
+function formatarDados(identificador, nome, email) {
+    console.log(identificador.padStart(6, '0'))
+
+    const arrayDeNomes = nome.split(' ')
+    let nomeFormatado = ''
+
+    for (let item of arrayDeNomes) {
+        let primeiraLetra = item[0]
+        let restoDoNome = item.slice(1)
+        nomeFormatado += primeiraLetra.toLocaleUpperCase() + restoDoNome + ' '
+    }
+    console.log(nomeFormatado)
+    console.log(email.trim())
+}
+
+formatarDados(identificador, nome, email)
