7be242ee6ef04c9422ae613adb00d2f9ae24c6e9
RalphCajazeira <ralphmtk@gmail.com>
2023-08-01 20:40:56 -0300

    08 - Remover pontuação

--- Aula-27-07-2023/exercicios-backend-metodos-strings/08/index.js
+++ Aula-27-07-2023/exercicios-backend-metodos-strings/08/index.js
@@ -1,1 +1,13 @@
 const cpf = "011.022.033-44";
+const cnpj = "12.345.678/0001-90";
+
+
+function deixarApenasNumeros(numero) {
+    const regex = /[^\d]/g; // Expressão regular para corresponder a qualquer caractere não numérico
+
+    const numeroSemPontuacao = numero.replace(regex, '');
+
+    return numeroSemPontuacao
+}
+
+console.log(deixarApenasNumeros(cnpj))
