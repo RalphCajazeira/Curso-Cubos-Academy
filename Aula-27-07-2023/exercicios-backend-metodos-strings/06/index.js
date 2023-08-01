6a1ba6c8dcf830f7db189522a94308004b79d3da
RalphCajazeira <ralphmtk@gmail.com>
2023-08-01 20:16:43 -0300

    06 - Formatar número de celular

--- Aula-27-07-2023/exercicios-backend-metodos-strings/06/index.js
+++ Aula-27-07-2023/exercicios-backend-metodos-strings/06/index.js
@@ -1,1 +1,30 @@
-const celular = 7199918888;
+const celular = 99918888;
+
+function formatarNumeroDDD(celular) {
+    let celularFormatado = celular.toString()
+
+    if (celularFormatado.length === 10) {
+        let ddd = celularFormatado.slice(0, 2); //71 9991 8888
+        let parte1 = celularFormatado.slice(2, 6);
+        let parte2 = celularFormatado.slice(6);
+        celularFormatado = `(${ddd}) 9 ${parte1}-${parte2}`;
+
+        return celularFormatado
+
+    } else if (celularFormatado.length === 8) {
+        let parte1 = celularFormatado.slice(0, 4); //71 9991 8888
+        let parte2 = celularFormatado.slice(4);
+        celularFormatado = `9 ${parte1}-${parte2}`;
+
+        return celularFormatado
+
+    } else {
+
+        return 'Numero invalido'
+    }
+
+}
+
+// if(celular.length)
+
+console.log(formatarNumeroDDD(celular))
