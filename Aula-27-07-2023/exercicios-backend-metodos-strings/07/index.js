4da14b3a3dc73c26576649dd6a0d086f55a92202
RalphCajazeira <ralphmtk@gmail.com>
2023-08-01 20:32:28 -0300

    07 - Validação de e-mail

--- Aula-27-07-2023/exercicios-backend-metodos-strings/07/index.js
+++ Aula-27-07-2023/exercicios-backend-metodos-strings/07/index.js
@@ -1,1 +1,16 @@
 const email = "aluno@cubos.academy";
+
+
+function verificarEmailValido(email) {
+    let indexArroba = email.indexOf('@');
+    let indexPonto = email.indexOf('.', indexArroba)
+    let comecaComPonto = email[0]
+    let terminaComPonto = email[email.length - 1]
+    if (email.includes('@') && indexPonto !== -1 && comecaComPonto !== '.' && terminaComPonto !== '.') {
+        return 'Esse é um email valido'
+    } else {
+        return 'Esse não é um email valido'
+    }
+}
+
+console.log(verificarEmailValido(email))
