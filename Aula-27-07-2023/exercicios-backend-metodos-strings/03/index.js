43424961cdfc2c6baad6860c1db6d65b9df284c7
RalphCajazeira <ralphmtk@gmail.com>
2023-08-01 19:20:09 -0300

    03 - URL amigável

--- Aula-27-07-2023/exercicios-backend-metodos-strings/03/index.js
+++ Aula-27-07-2023/exercicios-backend-metodos-strings/03/index.js
@@ -1,1 +1,15 @@
 const texto = "Aprenda programar do zero na Cubos Academy";
+
+
+
+function urlAmigavel(texto, substituir, substituto) {
+    while (texto !== texto.replace(substituir, substituto)) {
+        texto = texto.replace(substituir, substituto)
+    }
+    return texto
+}
+
+
+
+
+console.log(urlAmigavel(texto.toLowerCase(), ' ', '-'))
