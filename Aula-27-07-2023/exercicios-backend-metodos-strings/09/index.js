67c3ee71c1f360f859f6145715a73c46c197dd17
RalphCajazeira <ralphmtk@gmail.com>
2023-08-01 21:00:30 -0300

    09 - Gerador de Nickname

--- Aula-27-07-2023/exercicios-backend-metodos-strings/09/index.js
+++ Aula-27-07-2023/exercicios-backend-metodos-strings/09/index.js
@@ -1,1 +1,25 @@
 const nome = 'Guido Cerqueira';
+
+
+// function urlAmigavel(texto, substituir, substituto) {
+//     while (texto !== texto.replace(substituir, substituto)) {
+//         texto = texto.replace(substituir, substituto)
+//     }
+//     return texto
+// }
+
+
+
+// let nomeSemEspaco = urlAmigavel(nome, ' ', '')
+// nomeSemEspaco = (`@${nomeSemEspaco}`).slice(0, 13).toLowerCase()
+// console.log(nomeSemEspaco)
+
+function criarNickName(nome) {
+    let nomeSemEspaco = nome
+    nomeSemEspaco = (`@${nomeSemEspaco}`).replace(/\s/g, '').slice(0, 13).toLowerCase()
+    return nomeSemEspaco
+}
+
+
+
+console.log(criarNickName(nome))
