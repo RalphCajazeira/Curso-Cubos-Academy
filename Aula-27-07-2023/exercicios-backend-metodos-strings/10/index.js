9d76a524ae9522c1aa3f86327651b8a1bcdcccda
RalphCajazeira <ralphmtk@gmail.com>
2023-08-01 21:18:33 -0300

    10 - Validação de arquivos

--- Aula-27-07-2023/exercicios-backend-metodos-strings/10/index.js
+++ Aula-27-07-2023/exercicios-backend-metodos-strings/10/index.js
@@ -1,1 +1,25 @@
-const nomeArquivo = 'Foto da Familia.pdf';
+const nomeArquivo = 'Foto da Familia.png';
+
+
+function verificarSeEhImagem(nomeArquivo) {
+    const extencoesPermitidas = ['.jpg', '.jpeg', '.gif', '.png']
+    const extensaoArquivo = nomeArquivo.slice(nomeArquivo.lastIndexOf('.')).toLowerCase();
+    if (extencoesPermitidas.includes(extensaoArquivo)) {
+        console.log('Arquivo válido');
+    } else {
+        console.log('Arquivo inválido');
+    }
+
+}
+
+
+
+
+verificarSeEhImagem(nomeArquivo)
+
+// console.log('------------------------')
+// console.log(nomeArquivo.slice())
+// const pegarIndice = nomeArquivo.lastIndexOf('.')
+// const conter = nomeArquivo.slice(pegarIndice)
+// const verificador = nomeArquivo.includes(conter)
+// console.log(verificador)
