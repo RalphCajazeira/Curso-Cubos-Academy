06ff7f09d0cc70589237494d2213301d2982cd11
RalphCajazeira <ralphmtk@gmail.com>
2023-08-22 16:22:19 -0300

    Cirando Servidor

---
+++ Back End/Aula-17-08-2023-Meu-Primeiro-Servidor/Exercicio-Resolvido/src/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express()
+
+const usuarios = require('./usuarios');
+
+
+
+console.log(usuarios);
---
+++ Back End/Aula-17-08-2023-Meu-Primeiro-Servidor/Exercicio-Resolvido/src/usuarios.js
@@ -0,0 +1,29 @@
+const usuarios = [
+    {
+        id: 1,
+        nome: "João",
+        idade: 30,
+    },
+    {
+        id: 2,
+        nome: "Maria",
+        idade: 25,
+    },
+    {
+        id: 3,
+        nome: "José",
+        idade: 40,
+    },
+    {
+        id: 4,
+        nome: "Ana",
+        idade: 20,
+    },
+    {
+        id: 5,
+        nome: "Pedro",
+        idade: 35,
+    },
+];
+
+module.exports = usuarios
