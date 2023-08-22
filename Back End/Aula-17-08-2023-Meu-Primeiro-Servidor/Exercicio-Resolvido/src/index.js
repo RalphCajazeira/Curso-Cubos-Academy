bc3a933acbec997d473945b96215f50740144dae
RalphCajazeira <ralphmtk@gmail.com>
2023-08-22 16:42:27 -0300

    Clone dos Exercícios

--- Back End/Aula-17-08-2023-Meu-Primeiro-Servidor/Exercicio-Resolvido/src/index.js
+++ Back End/Aula-17-08-2023-Meu-Primeiro-Servidor/Exercicio-Resolvido/src/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
-const app = express()
+const servidor = express()
 
 const usuarios = require('./usuarios');
 
 
 
-console.log(usuarios);
+servidor.get('/usuarios', (requisicao, resposta) => {
+    resposta.send(usuarios)
+});
+
+
+servidor.listen(3000, () => {
+    console.log('Servidor inicializado na porta 3000');
+})
